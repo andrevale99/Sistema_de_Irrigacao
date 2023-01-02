110bb74cdebf4526e392ac24c2603db98fb3ac4c
andrevale99 <andrefvale@gmail.com>
2023-01-02 19:15:00 -0300

    Just main

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main()
+{
+    DDRB |= (1<<PB5);
+
+    for(;;)
+    {
+        PORTB ^= (1<<PB5);
+        _delay_ms(100);
+    }
+
+    return 0;
+}
