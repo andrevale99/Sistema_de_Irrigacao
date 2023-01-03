24c86addb535f3794ea5e1873a4677f1b408aa6a
andrevale99 <andrefvale@gmail.com>
2023-01-03 16:01:25 -0300

    Retirada da logica do RTC

--- src/main.c
+++ src/main.c
@@ -23,24 +23,9 @@
 
 #include <stdint.h>
 
-#define start_bit() TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN)
-#define stop_bit() TWCR = (1<<TWINT)|(1<<TWEN)| (1<<TWSTO)
-#define espera_envio() while (!(TWCR & (1<<TWINT)))
-#define envia_byte() TWCR = (1<<TWINT) | (1<<TWEN)
-#define recebe_byte() TWCR = (1<<TWINT) | (1<<TWEN)
-#define espera_recebimento() while (!(TWCR & (1<<TWINT)))
-#define recebe_byte_ret_nack() TWCR = (1<<TWINT) | (1<<TWEN)
-
-//A rotina de tratamento de erro é por conta do programador e da sua lógica de programação
-#define teste_envio_start() if((TWSR & 0xF8) != TW_START) PORTB |= (1<<PB5);
-#define teste_envio_end_escrita() if((TWSR & 0xF8) != TW_MT_SLA_ACK) PORTB |= (1<<PB5);
-#define teste_envio_dado() if((TWSR & 0xF8) != TW_MT_DATA_ACK) PORTB |= (1<<PB5);
-#define teste_envio_restart() if((TWSR & 0xF8) != TW_REP_START) PORTB |= (1<<PB5);
-#define teste_envio_end_leitura() if((TWSR & 0xF8) != TW_MR_SLA_ACK) PORTB |= (1<<PB5);
-#define teste_recebe_byte_ret_nack() if((TWSR & 0xF8) != TW_MR_DATA_NACK) PORTB |= (1<<PB5);
-
 #define SOLO PC0
 #define RELE PB0
+#define LED_ERRO PB5
 
 //===============================================
 //  VARIAVEIS
@@ -54,8 +39,6 @@ uint8_t adc_MSB = 0x00;
 void setup();
 uint16_t adc_conv(uint8_t pino);
 
-uint8_t ler_RTC(uint8_t endereco);
-void escreve_RTC(uint8_t dado, uint8_t endereco);
 //===============================================
 //  MAIN
 //===============================================
@@ -65,6 +48,7 @@ int main()
 
     for(;;)
     {
+        
     }
 
     return 0;
@@ -98,70 +82,4 @@ uint16_t adc_conv(uint8_t pino)
     ADCSRA |= (1<<ADSC);
 
     return (adc_MSB<<8) | adc_LSB;
-}
-
-uint8_t ler_RTC(uint8_t endereco)
-{
-    start_bit();
-    espera_envio();
-    teste_envio_start();
-    
-    TWDR = 0xD0; //carrega o endereço para acesso do DS1307 (bit0 = 0, escrita)
-                //para outro CI basta trocar este endereço
-
-    envia_byte();
-    espera_envio();
-    teste_envio_end_escrita();
-    
-    TWDR = endereco; //ajuste do ponteiro de endereço para a leitura do DS1307
-    
-    envia_byte();
-    espera_envio();
-    teste_envio_dado();
-    start_bit(); //reinício
-    espera_envio();
-    teste_envio_restart();
-    
-    TWDR = 0xD1; //carrega o endereço para acesso do DS1307 (bit0 = 1 é leitura)
-                //automaticamente o ATmega chaveia para o estado de recepção
-    
-    envia_byte();
-    espera_envio();
-    teste_envio_end_leitura();
-    
-    recebe_byte_ret_nack(); //só lê um byte, por isso retorna um NACK
-    espera_recebimento();
-    teste_recebe_byte_ret_nack();
-    
-    stop_bit();
-    
-    return TWDR; //retorna byte recebido
-}
-
-void escreve_RTC(uint8_t dado, uint8_t endereco)
-{
-    start_bit();
-    espera_envio();
-    teste_envio_start();
-    
-    TWDR = 0xD0; //carrega o endereço para acesso do DS1307 (bit0 = 0 é escrita)
-    //para outro CI basta trocar este endereço
-    
-    envia_byte();
-    espera_envio();
-    teste_envio_end_escrita();
-    
-    TWDR = endereco; //carrega o endereço para escrita do dado no DS1307
-    
-    envia_byte();
-    espera_envio();
-    teste_envio_dado();
-    
-    TWDR = dado; //carrega o dado para escrita no endereço especificado
-    
-    envia_byte();
-    espera_envio();
-    teste_envio_dado();
-    
-    stop_bit();
 }
