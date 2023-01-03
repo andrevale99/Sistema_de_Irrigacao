4792637c68fcba8c32ea179f5f6c79e1554e039b
andrevale99 <andrefvale@gmail.com>
2023-01-02 21:09:18 -0300

    Funcao para retornar a leitura analogica (sem interrupcao)

--- src/main.c
+++ src/main.c
@@ -2,13 +2,23 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+#include <stdint.h>
 
 #define SOLO ADC0
+
+//===============================================
+//  VARIAVEIS
+//===============================================
+uint8_t adc_LSB = 0x00;
+uint8_t adc_MSB = 0x00;
 
 //===============================================
 //  PROTOTIPOS
 //===============================================
 void setup();
+uint16_t adc_conv(uint8_t pino);
 
 //===============================================
 //  MAIN
@@ -19,8 +29,6 @@ int main()
 
     for(;;)
     {
-        PORTB ^= (1<<PB5);
-        _delay_ms(1000);
     }
 
     return 0;
@@ -32,4 +40,26 @@ int main()
 void setup()
 {
     DDRB |= (1<<PB5);
+
+    ADCSRA |= (1<<ADEN) | (1<<ADPS2) 
+              | (1<<ADPS1) | (1<<ADPS0);
+        
+    ADMUX |= (1<<REFS0);
+}
+
+uint16_t adc_conv(uint8_t pino)
+{
+    ADMUX |= pino;
+
+    ADCSRA |= (1<<ADSC);
+
+    while(!(ADCSRA &= ~(1<<ADIF)));
+    ADCSRA |= (1<<ADIF); 
+
+    adc_LSB = ADCL;
+    adc_MSB = ADCH;
+
+    ADCSRA |= (1<<ADSC);
+
+    return (adc_MSB<<8) | adc_LSB;
 }
