3329d6fd4ecaf797610d4027ce227b81ecae941a
andrevale99 <andrefvale@gmail.com>
2023-01-04 19:50:33 -0300

    Exclusao das funcoes de UART

--- src/main.c
+++ src/main.c
@@ -23,12 +23,7 @@
 #include <stdint.h>
 
 #define SOLO PC0
-#define POT PC1
 #define RELE PB0
-#define LED_ERRO PB5
-
-#define BAUD 9600
-#define MYUBRR F_CPU/16/BAUD-1
 
 //===============================================
 //  VARIAVEIS
@@ -42,8 +37,6 @@ uint8_t adc_MSB = 0x00;
 void setup();
 uint16_t adc_conv(uint8_t pino);
 
-void USART_Init(unsigned int ubrr);
-void USART_Transmit(unsigned char data);
 //===============================================
 //  MAIN
 //===============================================
@@ -87,24 +80,4 @@ uint16_t adc_conv(uint8_t pino)
     ADCSRA |= (1<<ADSC);
 
     return (adc_MSB<<8) | adc_LSB;
-}
-
-void USART_Init(unsigned int ubrr)
-{
-    /*Set baud rate */
-    UBRR0H = (unsigned char)(ubrr>>8);
-    UBRR0L = (unsigned char)ubrr;
-    /*Enable receiver and transmitter */
-    UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-    /* Set frame format: 8data, 2stop bit */
-    UCSR0C = (1<<USBS0)|(3<<UCSZ00);
-}
-
-void USART_Transmit(unsigned char data)
-{
-    /* Wait for empty transmit buffer */
-    while (!(UCSR0A & (1<<UDRE0))) 
-        ;
-    /* Put data into buffer, sends the data */
-    UDR0 = data;
 }
