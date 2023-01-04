351cc3245aab14e66d498bd0ce842badc7615c73
andrevale99 <andrefvale@gmail.com>
2023-01-04 15:38:34 -0300

    Funcoes de envio de dados pelo protocolo serial

--- src/main.c
+++ src/main.c
@@ -13,7 +13,6 @@
  *  e Marco V. M. Villaca
  * 
 */
-
 #define F_CPU 16000000UL
 
 #include <avr/io.h>
@@ -24,8 +23,12 @@
 #include <stdint.h>
 
 #define SOLO PC0
+#define POT PC1
 #define RELE PB0
 #define LED_ERRO PB5
+
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
 
 //===============================================
 //  VARIAVEIS
@@ -39,6 +42,8 @@ uint8_t adc_MSB = 0x00;
 void setup();
 uint16_t adc_conv(uint8_t pino);
 
+void USART_Init(unsigned int ubrr);
+void USART_Transmit(unsigned char data);
 //===============================================
 //  MAIN
 //===============================================
@@ -48,8 +53,7 @@ int main()
 
     for(;;)
     {
-        PORTB ^= (1<<PB5);
-        _delay_ms(250);
+  
     }
 
     return 0;
@@ -83,4 +87,24 @@ uint16_t adc_conv(uint8_t pino)
     ADCSRA |= (1<<ADSC);
 
     return (adc_MSB<<8) | adc_LSB;
+}
+
+void USART_Init(unsigned int ubrr)
+{
+    /*Set baud rate */
+    UBRR0H = (unsigned char)(ubrr>>8);
+    UBRR0L = (unsigned char)ubrr;
+    /*Enable receiver and transmitter */
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+    /* Set frame format: 8data, 2stop bit */
+    UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+
+void USART_Transmit(unsigned char data)
+{
+    /* Wait for empty transmit buffer */
+    while (!(UCSR0A & (1<<UDRE0))) 
+        ;
+    /* Put data into buffer, sends the data */
+    UDR0 = data;
 }
