2e1210e588d48a172c19652847b776cdff844937
andrevale99 <andrefvale@gmail.com>
2023-01-05 15:27:02 -0300

    Funcoes de inicializacao

--- src/i2c_lib.c
+++ src/i2c_lib.c
@@ -8,3 +8,16 @@ void i2c_init()
     TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWIE); //habilita o TWI e a interrupção
 }
 
+void i2c_init_pullup()
+{
+    I2C_DDR  &= ~((1<<SCL) | (1<<SDA));
+    I2C_PORT |= (1<<SCL) | (1<<SDA);
+
+    i2c_init(); 
+}
+
+void i2c_unit()
+{
+    TWCR &= ~(1<<TWEN);
+}
+
