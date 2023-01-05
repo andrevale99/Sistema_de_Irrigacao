94b36c7dda229ad0d2f90a9dbf647448453bd550
andrevale99 <andrefvale@gmail.com>
2023-01-05 15:47:02 -0300

    Funcao de transmissao de dados pela USART0

--- src/main.c
+++ src/main.c
@@ -27,11 +27,12 @@
 #define SOLO PC0
 #define RELE PB0
 
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
+
 //===============================================
 //  VARIAVEIS
 //===============================================
-uint8_t adc_LSB = 0x00;
-uint8_t adc_MSB = 0x00;
 
 //===============================================
 //  PROTOTIPOS
@@ -42,6 +43,9 @@ void gpio_setup();
 
 void adc_setup();
 uint16_t adc_conv(uint8_t pino);
+
+void USART_Transmit(unsigned char data); //Eniva caractere para a uart
+void USART_Init(unsigned int ubrr); //Inicializa a UART
 
 //===============================================
 //  MAIN
@@ -85,6 +89,9 @@ void adc_setup()
 
 uint16_t adc_conv(uint8_t pino)
 {
+    static uint8_t adc_LSB; 
+    static uint8_t adc_MSB;
+
     ADMUX |= pino;
 
     ADCSRA |= (1<<ADSC);
@@ -100,3 +107,22 @@ uint16_t adc_conv(uint8_t pino)
     return (adc_MSB<<8) | adc_LSB;
 }
 
+void USART_Init(unsigned int ubrr)
+{
+    /*Set baud rate */
+    UBRR0H = (unsigned char)(ubrr>>8);
+    UBRR0L = (unsigned char)ubrr;
+    /*Enable receiver and transmitter */
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+    /* Set frame format: 8data, 2stop bit */
+    UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+
+void USART_Transmit(unsigned char data)
+{
+    /* Wait for empty transmit buffer */
+    while (!(UCSR0A & (1<<UDRE0))) 
+        ;
+    /* Put data into buffer, sends the data */
+    UDR0 = data;
+}
