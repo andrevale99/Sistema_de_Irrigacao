4d23e68fea8cb07947f15f0e1a94ac8b37d4b74a
andrevale99 <andrefvale@gmail.com>
2023-01-05 20:17:18 -0300

    inline function

--- src/i2c_lib.c
+++ src/i2c_lib.c
@@ -1,6 +1,6 @@
 #include "i2c_lib.h"
 
-void i2c_init()
+inline void i2c_init()
 {
     //Ajuste da frequência de trabalho - SCL = F_CPU/(16+2.TWBR.Prescaler)
     TWBR = 18;
@@ -8,7 +8,7 @@ void i2c_init()
     TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN); //habilita o TWI
 }
 
-void i2c_init_pullup()
+inline void i2c_init_pullup()
 {
     I2C_DDR  &= ~((1<<SCL) | (1<<SDA));
     I2C_PORT |= (1<<SCL) | (1<<SDA);
@@ -16,12 +16,12 @@ void i2c_init_pullup()
     i2c_init(); 
 }
 
-void i2c_unit()
+inline void i2c_unit()
 {
     TWCR &= ~(1<<TWEN);
 }
 
-void i2c_start_bit()
+inline void i2c_start_bit()
 {
-    TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN)
+    TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
 }
