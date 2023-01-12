251749160e68bf2624609a459f081e2aad69ebb8
andrevale99 <andrefvale@gmail.com>
2023-01-12 12:50:05 -0300

    Correcao da logica do TesteBit

--- src/main.c
+++ src/main.c
@@ -33,7 +33,7 @@
 #define SetBit(port, pin) (port |= (1<<pin))
 #define ClrBit(port, pin) (port &= ~(1<<pin))
 #define ToggleBit(port, pin) (port ^= (1<<pin))
-#define TestBit(port, pin) (port &= (1<<pin))
+#define TestBit(port, pin) (port & (1<<pin))
 
 #define SOLO PC0
 #define RELE PD7
