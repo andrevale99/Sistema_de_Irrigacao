d36c4b7700f81312cc079fb4a1bd3bd741b5b102
andrevale99 <andrefvale@gmail.com>
2023-02-16 10:56:08 -0300

    inclusão da biblioteca Utilitarios.c

--- src/LCD.h
+++ src/LCD.h
@@ -10,10 +10,7 @@
 
 #include <stdint.h>
 
-#define SetBit(port, pin) (port |= (1<<pin))
-#define ClrBit(port, pin) (port &= ~(1<<pin))
-#define ToggleBit(port, pin) (port ^= (1<<pin))
-#define TestBit(port, pin) (port & (1<<pin))
+#include "Utilitarios.c"
 
 #define DADOS_LCD PORTD
 #define CONTR_LCD PORTB
--- src/main.c
+++ src/main.c
@@ -30,10 +30,7 @@
 #include "ds3231.h"
 #include "LCD.h"
 
-#define SetBit(port, pin) (port |= (1 << pin))
-#define ClrBit(port, pin) (port &= ~(1 << pin))
-#define ToggleBit(port, pin) (port ^= (1 << pin))
-#define TestBit(port, pin) (port & (1 << pin))
+#include "Utilitarios.c"
 
 #define SOLO PC0
 #define RELE PD2
