2efc5259fd4eaa617d26c9971b280dd8c7a52bf7
andrevale99 <andrefvale@gmail.com>
2023-02-16 08:44:51 -0300

    Mudança de pino e criação do define guard

--- src/LCD.h
+++ src/LCD.h
@@ -1,26 +1,30 @@
 #ifndef LCD_H
 #define LCD_h
 
-#define F_CPU 16000000
+#ifndef F_CPU
+    #define F_CPU 16000000UL
+#endif
 
 #include <avr/io.h>
 #include <util/delay.h>
+
+#include <stdint.h>
 
 #define SetBit(RES, BIT)(RES |= (1 << BIT)) // Por BIT em nível alto
 #define ClrBit(RES, BIT)(RES &= ~ (1 << BIT))// Por BIT em nível baixo
 #define TstBit(RES, BIT)(RES &  (1 << BIT)) // testar BIT, retorna 0 ou 1
 #define CplBit(RES, BIT)(RES ^= (1 << BIT)) // Inverter estado do BIT
 
-#define DADOS_LCD PORTB
-#define CONTR_LCD PORTB
-#define E PB4
-#define RS PB5
+#define DADOS_LCD PORTD
+#define CONTR_LCD PORTD
+#define E PB1
+#define RS PB0
 
 /*0 para via de dados do LCD nos 4 LSBs do PORT
 empregado (Px0-D4, Px1-D5, Px2-D6, Px3-D7), 1 para via de
 dados do LCD nos 4 MSBs do PORT empregado (Px4-D4, Px5-D5,
 Px6-D6, Px7-D7) */
-#define nibble_dados 0
+#define nibble_dados 1
 
 #define CLEAR_DISPLAY 0x01
 #define RETURN_HOME 0x02
